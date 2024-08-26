e52ada1ace9b9e63024f2dc515bc980e0936736c
abr-ya <gamer05@list.ru>
2024-08-26 22:12:24 +0300

    feat: Add SignIn and SignOut buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { PropsWithChildren } from "react";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import ConvexClerkProvider from "./ConvexClerkProvider";
 import { Footer, Header } from "./_components";
 
 const inter = Roboto({ subsets: ["latin"], weight: ["300", "400", "700", "900"] });
@@ -14,9 +15,11 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: PropsWithChildren) => (
   <html lang="en">
     <body className={inter.className}>
-      <Header />
-      <main className="flex min-h-[calc(100vh-275px)] flex-col items-center p-4">{children}</main>
-      <Footer />
+      <ConvexClerkProvider>
+        <Header />
+        <main className="flex min-h-[calc(100vh-275px)] flex-col items-center p-4">{children}</main>
+        <Footer />
+      </ConvexClerkProvider>
     </body>
   </html>
 );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,38 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-const Home = () => (
-  <>
-    <h1 className="text-2xl">Hello, Next 14 Files App!</h1>
-    <Button variant="destructive">I Button</Button>
-  </>
-);
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignOutButton, useSession } from "@clerk/nextjs";
+
+const Home = () => {
+  const { isLoaded, session } = useSession(); // isSignedIn
+  console.log(isLoaded, session?.publicUserData);
+
+  const renderHello = () => {
+    if (!session?.publicUserData) {
+      return "Hello, Guest from Next Files App!";
+    }
+
+    const { firstName, lastName } = session.publicUserData;
+
+    return `Hello, ${firstName} ${lastName} == Your Files will be here soon...`;
+  };
+
+  return (
+    <>
+      <h1 className="text-2xl">{renderHello()}</h1>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>SignIn</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <h2>{}</h2>
+        <SignOutButton>
+          <Button variant="destructive">SignOut</Button>
+        </SignOutButton>
+      </SignedIn>
+    </>
+  );
+};
 
 export default Home;

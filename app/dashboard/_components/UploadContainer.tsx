9cb28a21b3135f8b462699b30036ba06495a8094
abr-ya <gamer05@list.ru>
2024-09-03 21:53:55 +0300

    fix: Correct ownerId when file uploaded

--- app/dashboard/_components/UploadContainer.tsx
+++ app/dashboard/_components/UploadContainer.tsx
@@ -3,12 +3,25 @@ import { SchemaType } from "../formSchema";
 import UploadModal from "./UploadModal";
 import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
+import { useOrganization, useUser } from "@clerk/nextjs";
 
 const UploadContainer = () => {
+  const organization = useOrganization();
+  const user = useUser();
+
   const generateUploadUrl = useMutation(api.files.generateUploadUrl);
   const createFileMutation = useMutation(api.files.createFile);
 
+  let ownerId: string | undefined = undefined;
+  if (organization.isLoaded && user.isLoaded) {
+    ownerId = organization.organization?.id ?? user.user?.id;
+  }
+
   const submitHandler: ({ file, title }: SchemaType) => void = async ({ file, title }) => {
+    if (!ownerId) {
+      console.log("Cant get owner ID!");
+      return;
+    }
     const postUrl = await generateUploadUrl();
     const loadingFile = file[0];
 
@@ -25,16 +38,22 @@ const UploadContainer = () => {
 
     const types = {
       "image/png": "image",
+      "image/jpeg": "image",
       "application/pdf": "pdf",
       "text/csv": "csv",
     } as Record<string, Doc<"files">["type"]>;
+
+    if (!types[loadingFile.type]) {
+      console.log("Not found file type!");
+      return;
+    }
 
     try {
       await createFileMutation({
         name: title,
         fileId: storageId,
         type: types[loadingFile.type],
-        ownerId: "",
+        ownerId,
       });
 
       // todo: message!

7ab1397dd3f4195ed91ba69b7e15910719983d4f
abr-ya <gamer05@list.ru>
2024-09-03 09:28:56 +0300

    feat: Add Upload Request (fetch) to UploadContainer

--- app/dashboard/_components/UploadContainer.tsx
+++ app/dashboard/_components/UploadContainer.tsx
@@ -1,9 +1,40 @@
+import { useMutation } from "convex/react";
 import { SchemaType } from "../formSchema";
 import UploadModal from "./UploadModal";
+import { api } from "@/convex/_generated/api";
 
 const UploadContainer = () => {
-  const submitHandler: (data: SchemaType) => void = (data) => {
-    console.log(data);
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+  const createFileMutation = useMutation(api.files.createFile);
+
+  const submitHandler: ({ file, title }: SchemaType) => void = async ({ file, title }) => {
+    const postUrl = await generateUploadUrl();
+    const loadingFile = file[0];
+
+    console.log(loadingFile);
+
+    const result = await fetch(postUrl, {
+      method: "POST",
+      headers: { "Content-Type": loadingFile.type },
+      body: loadingFile,
+    });
+    const { storageId } = await result.json();
+
+    console.log(title, storageId);
+
+    try {
+      await createFileMutation({
+        name: title,
+        // fileId: storageId,
+        ownerId: "org_2lJtMxdJK4J4Hl9R3hA8xZwsl2V", // todo: use Really ID!!!
+      });
+
+      // todo: message!
+      console.log("success!!1");
+    } catch (err) {
+      // todo: message!
+      console.log("errorrrrrrr!!!");
+    }
   };
 
   return <UploadModal submitHandler={submitHandler} />;
